24d1b0ffbf5daa103a47f0c8afb954532584783a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-05 10:38:25 -0400

    stub the import

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,30 @@ module.exports = defineConfig({
     devServer: {
       framework: 'create-react-app',
       bundler: 'webpack',
+      webpackConfig: {
+        mode: 'development',
+        devtool: false,
+        module: {
+          rules: [
+            {
+              test: /\.?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react'],
+                  plugins: [
+                    [
+                      '@babel/plugin-transform-modules-commonjs',
+                      { loose: true },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
     },
   },
   e2e: {
--- src/App.cy.js
+++ src/App.cy.js
@@ -2,7 +2,16 @@
 import React from 'react'
 import './App.css'
 import App from './App'
+import * as Location from './Location'
 
 it('shows the location host and path', () => {
+  cy.stub(Location, 'getLocation')
+    .returns({
+      hostname: 'cy-test',
+      pathname: '/App',
+    })
+    .as('getLocation')
   cy.mount(<App />)
+  cy.contains('[data-cy=location]', 'cy-test /App')
+  cy.get('@getLocation').should('have.been.calledOnce')
 })
