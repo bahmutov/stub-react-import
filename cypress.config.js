ce9c9cf1e40272ccb8d0d44135c327f52b251e01
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-07 12:55:59 -0400

    add babel-plugin-istanbul to instrument code coverage

--- cypress.config.js
+++ cypress.config.js
@@ -10,14 +10,20 @@ module.exports = defineConfig({
         devtool: false,
         module: {
           rules: [
+            // application and Cypress files are bundled like React components
+            // and instrumented using the babel-plugin-istanbul
+            // (we will filter the code coverage for non-application files later)
             {
-              test: /\.?js$/,
+              test: /\.js$/,
               exclude: /node_modules/,
               use: {
                 loader: 'babel-loader',
                 options: {
                   presets: ['@babel/preset-env', '@babel/preset-react'],
                   plugins: [
+                    // we could optionally insert this plugin
+                    // only if the code coverage flag is on
+                    'istanbul',
                     [
                       '@babel/plugin-transform-modules-commonjs',
                       { loose: true },
