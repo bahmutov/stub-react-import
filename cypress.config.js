c5c3b2c2152f3dceeac9d9e11fcea5ab8099faf3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-07 13:19:01 -0400

    exclude the spec files from the code coverage

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   component: {
+    setupNodeEvents(on, config) {
+      console.log('setupNodeEvents for components')
+
+      // https://github.com/bahmutov/cypress-code-coverage
+      require('@bahmutov/cypress-code-coverage/plugin')(on, config)
+
+      return config
+    },
     devServer: {
       framework: 'create-react-app',
       bundler: 'webpack',
